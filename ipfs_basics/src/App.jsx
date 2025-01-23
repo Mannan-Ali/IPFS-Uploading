594153a0d5e67d277cfd952847fde2daeac681d1
mannan <alimannan.am244@gmail.com>
2025-01-23 15:42:51 +0530

    Add function to  upload on Pinata

--- ipfs_basics/src/App.jsx
+++ ipfs_basics/src/App.jsx
@@ -1,12 +1,55 @@
-import './App.css'
-import 'dotenv/config'
-function App() {
+import { useState } from "react";
+import "./App.css";
+import axios from "axios";
 
+
+function App() {
+  const [file, setfiles] = useState("");
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      // console.log(import.meta.env.VITE_PINATA_API_SECRET);
+      
+      //here we are adding the file data into fileData as the data is in formdata form hence new FormData
+      const fileData = new FormData();
+      fileData.append("file", file);
+
+      //this will have a url of where our file is uploaded on pinata 
+      const responseData = await axios({
+        method: "post",
+        url: "https://api.pinata.cloud/pinning/pinFileToIPFS",
+        headers: {
+          pinata_api_key: import.meta.env.VITE_PINATA_API_KEY,
+          pinata_secret_api_key: import.meta.env.VITE_PINATA_API_SECRET,
+          "Content-Type": "multipart/form-data",
+        },
+        data: fileData,
+      });
+      const fileUrl = "https://gateway.pinata.cloud/ipfs/" + responseData.data.IpfsHash;
+      console.log(fileUrl);
+      
+    } catch (error) {
+      console.log("Error occured while submitting: ", error);
+    }
+  };
   return (
     <>
-  
+      <h1>IPFS Tutorial - Upload your file </h1>
+      <form action="#">
+        <input
+          type="file"
+          onChange={(e) => {
+            //here we are taking the first file of all the files uploaded basically only one file can be uploaded at a time.
+            setfiles(e.target.files[0]);
+          }}
+        />
+        <button type="submit" onClick={handleSubmit}>
+          Upload
+        </button>
+      </form>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
