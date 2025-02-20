fd250df8489850d05df9916b0903ba324afd014e
mannan <alimannan.am244@gmail.com>
2025-02-20 12:49:23 +0530

    Add metadata to uploaded image

--- ipfs_basics/src/App.jsx
+++ ipfs_basics/src/App.jsx
@@ -2,20 +2,27 @@ import { useState } from "react";
 import "./App.css";
 import axios from "axios";
 
-
 function App() {
   const [file, setfiles] = useState("");
+  const [description, setSnapDescription] = useState("");
 
   const handleSubmit = async (e) => {
     e.preventDefault();
-    try {
-      // console.log(import.meta.env.VITE_PINATA_API_SECRET);
-      
-      //here we are adding the file data into fileData as the data is in formdata form hence new FormData
-      const fileData = new FormData();
-      fileData.append("file", file);
+    // console.log(import.meta.env.VITE_PINATA_API_SECRET);
+    //here we are adding the image file into fileData as pinata accpets multiform data request
+    const fileData = new FormData();
+    fileData.append("file", file); //appending image file to new form data
 
-      //this will have a url of where our file is uploaded on pinata 
+    //adding meta data to the file 
+    const metadata = JSON.stringify({
+      name: "Image Description",
+      keyvalues: {
+        description: description, //description we get from frontend
+      },
+    });
+    fileData.append("pinataMetadata", metadata);
+    //this will have a url of where our file is uploaded on pinata
+    try {
       const responseData = await axios({
         method: "post",
         url: "https://api.pinata.cloud/pinning/pinFileToIPFS",
@@ -26,9 +33,10 @@ function App() {
         },
         data: fileData,
       });
-      const fileUrl = "https://gateway.pinata.cloud/ipfs/" + responseData.data.IpfsHash;
+      console.log(responseData);
+      const fileUrl =
+        "https://gateway.pinata.cloud/ipfs/" + responseData.data.IpfsHash;
       console.log(fileUrl);
-      
     } catch (error) {
       console.log("Error occured while submitting: ", error);
     }
@@ -44,6 +52,19 @@ function App() {
             setfiles(e.target.files[0]);
           }}
         />
+        <div>
+          <label htmlFor="fname">Enter Description</label>
+          <input
+            type="text"
+            name="fname"
+            id="fname"
+            onChange={(e) => {
+              setSnapDescription(e.target.value);
+            }}
+            value={description}
+            placeholder="describe the event/img"
+          />
+        </div>
         <button type="submit" onClick={handleSubmit}>
           Upload
         </button>
